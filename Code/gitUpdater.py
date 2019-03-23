572ab973728b4d2e7df03ed44237d8b334889580
unknown <hamdialperencetin@gmail.com>
2019-03-23 13:28:13 +0300

    renamed variables

--- Code/gitUpdater.py
+++ Code/gitUpdater.py
@@ -28,9 +28,9 @@ for bug_report in bug_reports:
     git_checkout(commit)
     javaFiles = getAllCorpus()
 
-    date = convertToDateTime(report_time)
+    date = convert_to_datetime(report_time)
 
-    collaborativeFilterScore, bugFixingRecency_, bugFixingFrequency_ = None, None, None
+    collaborative_filter_score, bug_fixing_recency_, bug_fixing_frequency_ = None, None, None
     for buggy_src_file in files:
         buggy_src_file = os.path.normpath(buggy_src_file)
         try:
@@ -42,38 +42,38 @@ for bug_report in bug_reports:
         rVSMTextSimilarity = cosine_sim(bug_report["rawCorpus"], src)
 
         # Collaborative Filter Score
-        prevReports = getPreviousReportByFilename(buggy_src_file, date, bug_reports)
+        prevReports = get_previous_report_by_filename(buggy_src_file, date, bug_reports)
         relatedCorpus = []
         for report in prevReports:
             relatedCorpus.append(report["rawCorpus"])
         relatedString = ' '.join(relatedCorpus)
-        collaborativeFilterScore = cosine_sim(bug_report["rawCorpus"], relatedString)
+        collaborative_filter_score = cosine_sim(bug_report["rawCorpus"], relatedString)
 
         # Class Name Similarity
-        rawClassNames = src.split(" class ")[1:]
+        raw_class_names = src.split(" class ")[1:]
 
         classNames = []
-        for block in rawClassNames:
+        for block in raw_class_names:
             classNames.append(block.split(' ')[0])
         classCorpus = ' '.join(classNames)
         classNameSimilarity = cosine_sim(bug_report["rawCorpus"], classCorpus)
 
         # Bug Fixing Recency
-        mrReport = getMostRecentReport(buggy_src_file, convertToDateTime(bug_report["report_time"]), bug_reports)
-        bugFixingRecency_ = bugFixingRecency(bug_report, mrReport)
+        mrReport = get_most_recent_report(buggy_src_file, convert_to_datetime(bug_report["report_time"]), bug_reports)
+        bug_fixing_recency_ = bug_fixing_recency(bug_report, mrReport)
 
         # Bug Fixing Frequency
-        bugFixingFrequency_ = bugFixingFrequency(buggy_src_file, date, bug_reports)
+        bug_fixing_frequency_ = bug_fixing_frequency(buggy_src_file, date, bug_reports)
 
         our_features_path = os.path.normpath('./data/our_features.csv')
         with open(our_features_path, 'a', newline='') as f:
             writer = csv.writer(f)
-            writer.writerow([id, buggy_src_file, rVSMTextSimilarity, collaborativeFilterScore, classNameSimilarity, bugFixingRecency_, bugFixingFrequency_, 1])
+            writer.writerow([id, buggy_src_file, rVSMTextSimilarity, collaborative_filter_score, classNameSimilarity, bug_fixing_recency_, bug_fixing_frequency_, 1])
 
     for src_file in get_top_k_wrong_files(files, rawCorpus, javaFiles):
         with open(our_features_path, 'a', newline='') as f:
             writer = csv.writer(f)
-            writer.writerow([id, src_file[0], src_file[1], collaborativeFilterScore, src_file[2], bugFixingRecency_, bugFixingFrequency_, 0])
+            writer.writerow([id, src_file[0], src_file[1], collaborative_filter_score, src_file[2], bug_fixing_recency_, bug_fixing_frequency_, 0])
 print()
 
 # for bug_report_id, commit_id, description_terms, buggy_src_files:
--- Code/util.py
+++ Code/util.py
@@ -17,35 +17,35 @@ def tsv2dict():
     for line in reader:
         line["files"] = [s.strip() + ".java" for s in line["files"].split(".java") if s]
         line["rawCorpus"] = line["summary"][10:] + line["description"]
-        line["summary"], line["description"], combinedCorpus = getCombinedCorpus(line)
+        line["summary"], line["description"], combined_corpus = get_combined_corpus(line)
 
         dict_list.append(line)
     return dict_list
 
 
-def getCombinedCorpus(report):
-    report["summary"] = cleanAndSplit(report["summary"])[2:]  
-    report["description"] = cleanAndSplit(report["description"])
-    combinedCorpus = report["summary"] + report["description"]
-    return report["summary"], report["description"], combinedCorpus
+def get_combined_corpus(report):
+    report["summary"] = clean_and_split(report["summary"])[2:]  
+    report["description"] = clean_and_split(report["description"])
+    combined_corpus = report["summary"] + report["description"]
+    return report["summary"], report["description"], combined_corpus
 
-def cleanAndSplit(text):
+def clean_and_split(text):
     
-    returnText = re.sub(r'[^\w\s]','',text)
+    return_text = re.sub(r'[^\w\s]','',text)
 
-    returnText = [s.strip() for s in returnText.split()]
-    return returnText
+    return_text = [s.strip() for s in return_text.split()]
+    return return_text
 
 
 def csv2dict():
     file_ = os.path.normpath('./data/features.csv')
     with open(file_, 'r') as f:
         reader = csv.DictReader(f, delimiter=',') 
-        csvDict = list()
+        csv_dict = list()
         for line in reader:
-            csvDict.append(line)   
+            csv_dict.append(line)   
                      
-    return csvDict
+    return csv_dict
 
 def git_checkout(commmit_id):
     eclipse_ui_git_directory = os.path.normpath("./data/eclipse.platform.ui")
@@ -55,64 +55,64 @@ def git_checkout(commmit_id):
     os.system("git checkout " + str(next_commit_id))
     os.chdir(os.path.normpath("../.."))
 
-def get_top_k_wrong_files(rightFiles, brCorpus, javaFiles):
-    randomlySampled = random.sample(list(javaFiles), 100)  
+def get_top_k_wrong_files(right_files, br_corpus, java_files):
+    randomly_sampled = random.sample(list(java_files), 100)  
 
-    allFiles = []
-    for filename in [f for f in randomlySampled if f not in rightFiles]:
+    all_files = []
+    for filename in [f for f in randomly_sampled if f not in right_files]:
         try:
-            rawClassNames = javaFiles[filename].split(" class ")[1:]
+            raw_class_names = java_files[filename].split(" class ")[1:]
 
-            classNames = []
-            for block in rawClassNames:
-                classNames.append(block.split(' ')[0])
-            classCorpus = ' '.join(classNames)
+            class_names = []
+            for block in raw_class_names:
+                class_names.append(block.split(' ')[0])
+            class_corpus = ' '.join(class_names)
 
-            one = cosine_sim(brCorpus, javaFiles[filename])
-            two = cosine_sim(brCorpus, classCorpus)
+            one = cosine_sim(br_corpus, java_files[filename])
+            two = cosine_sim(br_corpus, class_corpus)
 
-            fileInfo = [filename, one, two]
-            allFiles.append(fileInfo)
+            file_info = [filename, one, two]
+            all_files.append(file_info)
         except Exception:
             print("Error in wrong file parsing")
-            del javaFiles[filename]
+            del java_files[filename]
 
-    topfifty = sorted(allFiles, key=lambda x: x[1], reverse=True)[:len(rightFiles)]
+    topfifty = sorted(all_files, key=lambda x: x[1], reverse=True)[:len(right_files)]
     return topfifty
 
-def calculate_scores(id, buggy_src_file, javaFiles, rawCorpus, date, bug_report, bug_reports, report_time, match):
+def calculate_scores(id, buggy_src_file, java_files, raw_corpus, date, bug_report, bug_reports, report_time, match):
     buggy_src_file = os.path.normpath(buggy_src_file)
     try:
-        src = javaFiles[buggy_src_file]
+        src = java_files[buggy_src_file]
     except:
         return
 
-    rVSMTextSimilarity = cosine_sim(rawCorpus, src)
+    rVSM_text_similarity = cosine_sim(raw_corpus, src)
 
-    prevReports = getPreviousReportByFilename(buggy_src_file, date, bug_reports)
-    relatedCorpus = []
-    for report in prevReports:
-        relatedCorpus.append(report["rawCorpus"])
-    relatedString = ' '.join(relatedCorpus)
-    collaborativeFilterScore = cosine_sim(rawCorpus, relatedString)
+    prev_reports = get_previous_report_by_filename(buggy_src_file, date, bug_reports)
+    related_corpus = []
+    for report in prev_reports:
+        related_corpus.append(report["rawCorpus"])
+    related_string = ' '.join(related_corpus)
+    collaborative_filter_score = cosine_sim(raw_corpus, related_string)
 
-    rawClassNames = src.split(" class ")[1:]
+    raw_class_names = src.split(" class ")[1:]
 
-    classNames = []
-    for block in rawClassNames:
-        classNames.append(block.split(' ')[0])
-    classCorpus = ' '.join(classNames)
-    classNameSimilarity = cosine_sim(bug_report["rawCorpus"], classCorpus)
+    class_names = []
+    for block in raw_class_names:
+        class_names.append(block.split(' ')[0])
+    class_corpus = ' '.join(class_names)
+    classname_similarity = cosine_sim(bug_report["rawCorpus"], class_corpus)
 
-    mrReport = getMostRecentReport(buggy_src_file, convertToDateTime(report_time), bug_reports)
-    bugFixingRecency_ = bugFixingRecency(bug_report, mrReport)
+    mrReport = get_most_recent_report(buggy_src_file, convert_to_datetime(report_time), bug_reports)
+    bug_fixing_recency_ = bug_fixing_recency(bug_report, mrReport)
 
-    bugFixingFrequency_ = bugFixingFrequency(buggy_src_file, date, bug_reports)
+    bug_fixing_frequency_ = bug_fixing_frequency(buggy_src_file, date, bug_reports)
 
     our_features_path = os.path.normpath('./data/our_features.csv')
     with open(our_features_path, 'a', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow([id, buggy_src_file, rVSMTextSimilarity, collaborativeFilterScore, classNameSimilarity, bugFixingRecency_, bugFixingFrequency_, match])
+        writer.writerow([id, buggy_src_file, rVSM_text_similarity, collaborative_filter_score, classname_similarity, bug_fixing_recency_, bug_fixing_frequency_, match])
 
 stemmer = PorterStemmer()
 remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
@@ -133,45 +133,45 @@ files = {}
 start_dir = os.path.normpath("./data/eclipse.platform.ui")
 
 def getAllCorpus():
-    for dir,dirNames,fileNames in os.walk(start_dir):
-        for filename in [f for f in fileNames if f.endswith(".java")]:
-            srcName = os.path.join(dir, filename)
-            srcFile = open(srcName, 'r')
-            src = srcFile.read()
-            srcFile.close()
+    for dir, dir_names, file_names in os.walk(start_dir):
+        for filename in [f for f in file_names if f.endswith(".java")]:
+            src_name = os.path.join(dir, filename)
+            src_file = open(src_name, 'r')
+            src = src_file.read()
+            src_file.close()
 
-            fileKey = srcName.split(start_dir)[1]
-            fileKey = fileKey[len(os.sep):]
-            files[fileKey] = src
+            file_key = src_name.split(start_dir)[1]
+            file_key = file_key[len(os.sep):]
+            files[file_key] = src
 
     return files
 
-def getPreviousReportByFilename(filename, brdate, dictionary):
-    return [br for br in dictionary if (filename in br["files"] and convertToDateTime(br["report_time"]) < brdate)]
+def get_previous_report_by_filename(filename, brdate, dictionary):
+    return [br for br in dictionary if (filename in br["files"] and convert_to_datetime(br["report_time"]) < brdate)]
 
-def convertToDateTime(date):
+def convert_to_datetime(date):
     return datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
 
-def getMonthsBetween(d1, d2):
-    date1 = convertToDateTime(d1)
-    date2 = convertToDateTime(d2)
+def get_months_between(d1, d2):
+    date1 = convert_to_datetime(d1)
+    date2 = convert_to_datetime(d2)
     return abs((date1.year - date2.year) * 12 + date1.month - date2.month)
 
-def getMostRecentReport(filename, currentDate, dictionary):
-    matchingReports = getPreviousReportByFilename(filename, currentDate, dictionary)
-    if len(matchingReports) > 0:
-        return max((br for br in matchingReports), key=lambda x:convertToDateTime(x.get("report_time")))
+def get_most_recent_report(filename, current_date, dictionary):
+    matching_reports = get_previous_report_by_filename(filename, current_date, dictionary)
+    if len(matching_reports) > 0:
+        return max((br for br in matching_reports), key=lambda x:convert_to_datetime(x.get("report_time")))
     else:
         return None
 
-def bugFixingRecency(report1, report2):
+def bug_fixing_recency(report1, report2):
     if report1 is None or report2 is None:
         return 0
     else:
-        return 1/float(getMonthsBetween(report1.get("report_time"), report2.get("report_time")) + 1)
+        return 1/float(get_months_between(report1.get("report_time"), report2.get("report_time")) + 1)
 
-def bugFixingFrequency(filename, date, dictionary):
-    return len(getPreviousReportByFilename(filename, date, dictionary))
+def bug_fixing_frequency(filename, date, dictionary):
+    return len(get_previous_report_by_filename(filename, date, dictionary))
 
-def collaborativeFilteringScore(report, filename, dictionary):
-    matchingReports = getPreviousReportByFilename(filename, convertToDateTime(report.get("report_time")), dictionary)
+def collaborative_filtering_score(report, filename, dictionary):
+    matching_reports = get_previous_report_by_filename(filename, convert_to_datetime(report.get("report_time")), dictionary)
