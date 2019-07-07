446bc5c89cc59d0740c151ac2d0361936fc01bdc
unknown <hamdialperencetin@gmail.com>
2019-07-07 23:00:34 +0300

    removed duplicate parts

--- src/dnn_model.py
+++ src/dnn_model.py
@@ -84,9 +84,7 @@ def train_dnn(
     )
     clf.fit(X_train, y_train.ravel())
 
-    predicted = clf.predict(X_test)
-
-    ###########
+    # predicted = clf.predict(X_test)
 
     topk_counters = [0] * 20
     negative_total = 0
@@ -153,72 +151,3 @@ acc_dicts = Parallel(n_jobs=cpu_count() - 1)(
 )
 
 print(acc_dicts)
-
-# Testing
-for hidden_node_count in range(100, 1001, 100):
-    print("Hidden node count", hidden_node_count)
-    clf = MLPRegressor(
-        solver="sgd",
-        alpha=1e-5,
-        hidden_layer_sizes=(hidden_node_count,),
-        random_state=1,
-        max_iter=10000,
-        n_iter_no_change=30,
-    )
-    clf.fit(features_train, labels_train.ravel())
-
-    # predicted = clf.predict(data_test)
-
-    sample_dict = {}
-
-    for sample in samples_:
-        sample_dict[sample["report_id"]] = []
-
-    for sample in samples_:
-        temp_dict = {}
-        temp_dict[sample["file"]] = [
-            float(sample["rVSM_similarity"]),
-            float(sample["collab_filter"]),
-            float(sample["classname_similarity"]),
-            float(sample["bug_recency"]),
-            float(sample["bug_frequency"]),
-        ]
-
-        sample_dict[sample["report_id"]].append(temp_dict)
-
-    bug_reports = tsv2dict("../data/Eclipse_Platform_UI.txt")
-    bug_reports_files_dict = {}
-
-    for bug_report in bug_reports:
-        bug_id = bug_report["id"]
-        bug_reports_files_dict[bug_id] = bug_report["files"]
-
-    topk_counters = [0] * 20
-    negative_total = 0
-    for bug_report in bug_reports:
-        dnn_input = []
-        corresponding_files = []
-        bug_id = bug_report["id"]
-
-        try:
-            for temp_dict in sample_dict[bug_id]:
-                key = list(temp_dict.keys())[0]
-                value = list(temp_dict.values())[0]
-                dnn_input.append(value)
-                corresponding_files.append(key)
-        except:
-            negative_total += 1
-            continue
-
-        relevancy_list = clf.predict(dnn_input)
-
-        for i in range(1, 21):
-            max_indices = np.argpartition(relevancy_list, -i)[-i:]
-            for corresponding_file in np.array(corresponding_files)[max_indices]:
-                if str(corresponding_file) in bug_reports_files_dict[bug_id]:
-                    topk_counters[i - 1] += 1
-                    break
-
-    for i, counter in enumerate(topk_counters):
-        acc = counter / (len(bug_reports) - negative_total)
-        print("Accuracy of top", i + 1, ":", acc)
